004185b88361dd15f43bcf6df538298d9795c8c9
Rodrigo Drumond de Paula <r-drumond@hotmail.com>
2018-11-15 23:52:05 -0200

    primeiro commit
    compilador com analisador lexico e semantico os dois com tratamento de erro modo panico

---
+++ Portugolo/src/compilador/Identificador.java
@@ -0,0 +1,23 @@
+package compilador;
+
+/**
+ * InnerInfIdentificador
+ */
+public class Identificador {
+	private String tipo;
+
+	public  Identificador(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+	
+}
---
+++ Portugolo/src/compilador/Portugolo.java
@@ -0,0 +1,18 @@
+package compilador;
+
+public class Portugolo {
+
+	public static void main(String[] args) {
+
+		Lexema lexer = new Lexema("portugolo\\primeiro_portugolo.ptgl"); // diretorio
+
+		Parser parser = new Parser(lexer);
+		parser.Compilador();
+		parser.fechaArquivo();
+
+		lexer.printTS();
+
+		System.out.println("Compilação realizada do portugolo");
+	}
+
+}
